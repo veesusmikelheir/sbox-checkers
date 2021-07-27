9123a41bad209b5c03105e89cf879b3e390cafdd
Daniel Shemesh <memebigboy27@hotmail.com>
2021-07-27 10:46:15 -0400

    made eyerot change

--- code/CheckersPlayer.cs
+++ code/CheckersPlayer.cs
@@ -9,10 +9,24 @@ namespace Checkers
 {
     public partial class CheckersPlayer : Player
     {
+        public override void FrameSimulate(Client cl)
+        {
+            base.FrameSimulate(cl);
+            if (cl.Pawn == null) return;
+            cl.Pawn.EyeRot = Input.Rotation;
+        }
+
         public override void Respawn()
         {
             base.Respawn();
             Camera = new FunnyCamera();
+        }
+
+        public override void Simulate(Client cl)
+        {
+            base.Simulate(cl);
+            if (cl.Pawn == null) return;
+            cl.Pawn.EyeRot = Input.Rotation;
         }
     }
 }
